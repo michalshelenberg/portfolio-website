420cb15b8916695aef99250edf7be174a3750dfe
Michal Shelenberg <michalshelenberg@outlook.com>
2026-02-07 18:54:16 +0100

    feat: initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+
+export default function Page() {
+  return (
+    <section>
+      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
+        Michal Shelenberg
+      </h1>
+      <p className="mb-4">
+        Web developer specializing in developing, designing, and testing modern web applications with React, TypeScript, and Next.js. Experienced in styling with SASS and Tailwind CSS, and proficient across multiple programming languages.
+      </p>
+      {/* <div className="my-8">
+        <BlogPosts />
+      </div> */}
+    </section>
+  )
+}
